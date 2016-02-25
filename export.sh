564a8a82383b9ad675dfbebc1dbb7a8bc2ad1a59
poufpoufproduction <johannc@poufpoufproduction.fr>
2016-02-25 12:39:02 +0100

    300 cliparts

--- export.sh
+++ export.sh
@@ -4,7 +4,7 @@ rm -rf ../.tmp
 mkdir ../.tmp
 
 for f in */*.svg; do
-	inkscape $f -e ../.tmp/`basename -s .svg $f`.png -d 72 -h 150 -w 150
+	inkscape $f -e ../.tmp/`basename -s .svg $f`.png -d 72 -h 200 -w 200
 done
 
 montage -tile 20x -geometry +0+0 ../.tmp/* export.png
